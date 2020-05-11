bfb0394a029f6f1bc6f68691c4346c5399e1a930
Juan Pablo Chaclan <jchaclan@gmail.com>
2020-05-11 13:00:57 +0200

    Tree version

---
+++ tree.py
@@ -0,0 +1,17 @@
+picture = [
+  [0, 0, 0, 1, 0, 0, 0],
+  [0, 0, 1, 1, 1, 0, 0],
+  [0, 1, 1, 1, 1, 1, 0],
+  [1, 1, 1, 1, 1, 1, 1],
+  [0, 0, 0, 1, 0, 0, 0],
+  [0, 0, 0, 1, 0, 0, 0]
+]
+
+
+for image in picture:
+        line = ''
+        for pixel in image:
+        color = '*' if pixel == 1 else ' '
+        line += color
+    print(line)
+print ('')
