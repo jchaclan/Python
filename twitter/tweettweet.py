6ee04def39611dce0b41999420e0a76c15d8d1b2
Juan Pablo Chaclan <jchaclan@gmail.com>
2020-05-30 19:25:03 +0200

    you don't have the correct token

--- twitter/tweettweet.py
+++ twitter/tweettweet.py
@@ -1,10 +1,10 @@
 import tweepy
 import time
 
-API_key = 'bXr3IlsKkpRygs4hmL54qHZAd'
-API_key_secret = '0qYMqx4d6YrTHtkCoVoHfkIFCCKvldTeILyfySdQSsMZSVLqCj'
-Access_token = '1265354860238704641-WkkkcVhPDkiULfDus62tEKJ9B4p9d1'
-Access_token_secret = 'TSlV4zqumG9AJNRlWq3Eqcfd8gARD3ksynbDSwJhUFf91'
+API_key = 'bXr3IlsKkpRygs4hmL54qHZA'
+API_key_secret = '0qYMqx4d6YrTHtkCoVoHfkIFCCKvldTeILyfySdQSsMZSVLqC'
+Access_token = '1265354860238704641-WkkkcVhPDkiULfDus62tEKJ9B4p9d'
+Access_token_secret = 'TSlV4zqumG9AJNRlWq3Eqcfd8gARD3ksynbDSwJhUFf9'
 
 auth = tweepy.OAuthHandler(API_key, API_key_secret)
 auth.set_access_token(Access_token,Access_token_secret)
